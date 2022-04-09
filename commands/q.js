11bb4468d74db1b5b3d6a4e4c177e7d7a6d96d1e
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2022-04-09 13:52:09 -0500

    refactor queue countdown

--- commands/q.js
+++ commands/q.js
@@ -3,11 +3,16 @@ export default {
     auth: ['owner'],
     execute(client, channel, tags, message, args) {
         const delay = 1500;
-        client.say(channel, 'Queues starting soon!');
-        // TODO write something that doesn't make my eyes bleed thanks
-        setTimeout(() => { client.say(channel, '3...'); }, delay);
-        setTimeout(() => { client.say(channel, '2...'); }, delay * 2);
-        setTimeout(() => { client.say(channel, '1...'); }, delay * 3);
-        setTimeout(() => { client.say(channel, 'Queue!'); }, delay * 4);
+        const sleep = ms => { return new Promise(resolve => setTimeout(resolve, ms)) }
+
+        const countDown = async i => {
+            client.say(channel, 'Queues are starting soon!')
+            while (i > 0) {
+                client.say(channel, `${i--}...`)
+                await sleep(delay);
+            }
+            client.say(channel, 'Queue up!!')
+        }
+        countDown(3);
     }
 }
