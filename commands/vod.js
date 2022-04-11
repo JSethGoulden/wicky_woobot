eb2daaec3e124184da107afcad9174de54181b5a
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2022-04-11 11:48:01 -0500

    add more vods

--- commands/vod.js
+++ commands/vod.js
@@ -2,12 +2,30 @@ export default {
     name: 'vod',
     auth: [],
     execute(client, channel, tags, message, args) {
-        if (args.length == 0) return;
+        if (args.length == 0) client.say(channel, `Usage: !${this.name} <vod name>`);
 
         const vods = {
-            'p1s': 'https://www.twitch.tv/videos/1257866068',
+            'tea': 'https://www.twitch.tv/videos/540833355',
+
+            'p4s': 'clear wasn\'t recorded :(',
+            'p3s': 'https://www.twitch.tv/videos/1257862635',
             'p2s': 'https://www.twitch.tv/videos/1257867175',
-            'p3s': 'https://www.twitch.tv/videos/1257862635'
+            'p1s': 'https://www.twitch.tv/videos/1257866068',
+
+            'e12s': 'https://www.twitch.tv/videos/854112722',
+            'e11s': 'https://www.twitch.tv/videos/835760517',
+            'e10s': 'https://www.twitch.tv/videos/835749412',
+            'e9s': 'https://www.twitch.tv/videos/835738691',
+
+            'e8': 'https://www.twitch.tv/videos/565213050',
+            'e7s': 'https://www.twitch.tv/videos/556458291',
+            'e6s': 'https://www.twitch.tv/videos/555973733',
+            'e5s': 'https://www.twitch.tv/videos/555972688',
+
+            'e4s': 'clear wasn\'t recorded :(',
+            'e3s': 'https://www.twitch.tv/videos/461841933',
+            'e2s': 'https://www.twitch.tv/videos/461388286',
+            'e1s': 'https://www.twitch.tv/videos/461388857',
         }
 
         const vod = vods[args[0].toLowerCase()] ?? 'Vod not found NotLikeThis';
