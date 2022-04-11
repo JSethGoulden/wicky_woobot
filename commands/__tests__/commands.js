e1ee5377e13436729cd0ec382738588ec4909d34
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2022-04-11 16:58:55 -0500

    more tests

--- commands/__tests__/commands.js
+++ commands/__tests__/commands.js
@@ -12,6 +12,11 @@ const tags = {
 }
 const message = '';
 const args = [];
+
+beforeEach(() => {
+    client.say.mockClear();
+});
+
 describe('chat commands', () => {
     describe('q', () => {
         test('sends exactly 5 chat messages', async () => {
@@ -19,4 +24,20 @@ describe('chat commands', () => {
             expect(client.say.mock.calls.length).toBe(5);
         });
     });
+    describe('vod', () => {
+        test('sends exactly 1 chat message', () => {
+            vod.execute(client, channel, tags, message, ['p1s']);
+            expect(client.say.mock.calls.length).toBe(1);
+        });
+
+        test('sends an error message if the vod is not found', () => {
+            vod.execute(client, channel, tags, message, ['notfound']);
+            expect(client.say.mock.results[0].value).toBe('Vod not found NotLikeThis');
+        });
+
+        test('sends an appropriate link or message if the vod is found', () => {
+            vod.execute(client, channel, tags, message, ['p1s']);
+            expect(client.say.mock.results[0].value).not.toBe('Vod not found NotLikeThis');
+        })
+    })
 });
